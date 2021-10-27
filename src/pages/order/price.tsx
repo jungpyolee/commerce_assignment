c203df4115c974eeb8955ccb7c80c1761738923d
jungpyolee <jungpyo89@gmail.com>
2021-10-27 16:42:02 +0900

    feat: add orderPrice component

---
+++ src/pages/order/price.tsx
@@ -0,0 +1,29 @@
+import { itemState, priceState } from '@atoms';
+import { useRecoilState } from 'recoil';
+import React from 'react';
+import { currency } from '@js/utils';
+
+export const Price = () => {
+  const [price, setPrice] = useRecoilState(priceState);
+
+  return (
+    <div className="w-full h-52 bg-white mt-2 p-5">
+      <div className="flex justify-between mt-2">
+        <p>총 상품금액</p>
+        <p>{currency(price.total)} 원</p>
+      </div>
+      <div className="flex justify-between mt-2">
+        <p>총 배송비 (3만원 이상 결제 시 무료)</p>
+        <p>{price.total - price.discount >= 30000 ? '0 원' : `+ ${currency(3000)}원`}</p>
+      </div>
+      <div className="flex justify-between mt-2">
+        <p>총 할인금액</p>
+        <p>- {currency(price.discount)} 원</p>
+      </div>
+      <div className="flex justify-between mt-4">
+        <p className="text-lg">결제금액</p>
+        <p className="text-xl font-bold">{currency(price.final)} 원</p>
+      </div>
+    </div>
+  );
+};
