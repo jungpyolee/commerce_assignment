ac10b46adeb433b362f8f63ce349f0313286e70b
jungpyolee <jungpyo89@gmail.com>
2021-11-02 02:01:53 +0900

    feat: add APIs (getItems... cart CUD)

--- src/common/api/index.ts
+++ src/common/api/index.ts
@@ -30,17 +30,26 @@ export const userMeApi = () => API.get<CurrentUser>('/users/me');
 // export const { query: getCategories, get: getCategory } = ApiService('categories');
 
 export const getItems = (params = null) => API.get<any>('/items', { params });
+export const getItemsByName = (params) => API.get<any>(`items?q[name_cont]=${params}`);
+export const getItemsByCategoryId = (params) => API.get<any>(`items?q[category_id_eq]=${params}`);
 export const getItem = (itemId) => API.get<any>(`/items/${itemId}`);
 
 export const getCategories = (params = null) => API.get<Category[]>('/categories', { params });
 export const getCategory = (id, params = null) => API.get<Category>(`/categories/${id}`, { params });
 
 export const getCart = (params = null) => API.get<any>('/cart', { params });
+export const addCart = (id, count) => API.post('/line_items', { item_id: id, quantity: count });
+export const updateCart = (id, quantity) => API.patch(`/line_items/${id}`, { quantity: quantity });
+export const deleteCart = (id) => API.delete(`/line_items/${id}`);
 
 export const getPosts = () => async (params = null) => {
   const { data } = await API.get('/posts', { params });
   return data;
 };
+
+export const getOrders = () => API.get<any>('/orders');
+export const getOrder = (id) => API.get<any>(`/orders/${id}`);
+export const updateOrder = (id, body) => API.patch(`/orders/${id}`, body);
 export const getPost = (postId) => async () => {
   const { data } = await API.get<any>(`/posts/${postId}`);
   return data;
