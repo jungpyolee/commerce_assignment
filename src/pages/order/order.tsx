e7f3e42271b9b348c78994aa2bb9810f09f34aa9
jungpyolee <jungpyo89@gmail.com>
2021-11-02 02:16:31 +0900

    fix: send orderId by props to Form

--- src/pages/order/order.tsx
+++ src/pages/order/order.tsx
@@ -1,17 +1,22 @@
 import { AccordionContent, Block, Button, f7, Icon, Link, List, ListItem, Navbar, Page } from 'framework7-react';
-import { PageRouteProps } from '@constants';
-import { API_URL, getCart, getItem } from '@api';
+import { Order, PageRouteProps } from '@constants';
+import { API_URL, getCart, getItem, getOrder } from '@api';
 import React, { useEffect, useState } from 'react';
 import OrderForm from './OrderForm';
 
 const OrderPage = ({ f7route, f7router }: PageRouteProps) => {
-  const [order, setOrder] = useState({});
+  const [orderId, setOrderId] = useState<number>();
 
+  useEffect(() => {
+    getCart().then((res) => {
+      setOrderId(res.data.id);
+    });
+  }, []);
   return (
     <Page className="bg-gray-200" noToolbar>
       <Navbar title="주문/결제" backLink />
 
-      <OrderForm f7router={f7router} setOrder={setOrder} />
+      <OrderForm f7router={f7router} orderId={orderId} />
     </Page>
   );
 };
