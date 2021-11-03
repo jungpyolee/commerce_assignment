c779991671d301a56dbcad7ae670245ce91c1a89
jungpyolee <jungpyo89@gmail.com>
2021-11-04 01:52:13 +0900

    fix: price Component UI

--- src/pages/order/price.tsx
+++ src/pages/order/price.tsx
@@ -7,22 +7,22 @@ export const Price = () => {
   const [price, setPrice] = useRecoilState(priceState);
 
   return (
-    <div className="w-full h-52 bg-white mt-2 p-5">
+    <div className="w-full h-44 bg-white mt-2 p-5">
       <div className="flex justify-between mt-2">
         <p>총 상품금액</p>
         <p>{currency(price.total)} 원</p>
       </div>
       <div className="flex justify-between mt-2">
-        <p>총 배송비 (3만원 이상 결제 시 무료)</p>
-        <p>{price.total - price.discount >= 30000 ? '0 원' : `+ ${currency(3000)}원`}</p>
+        <p>배송비</p>
+        <p>{price.total - price.discount >= 30000 ? '0 원' : `+ ${currency(3000)} 원`}</p>
       </div>
       <div className="flex justify-between mt-2">
         <p>총 할인금액</p>
-        <p>- {currency(price.discount)} 원</p>
+        <p>{price.discount ? `-${currency(price.discount)} 원` : '-'} </p>
       </div>
       <div className="flex justify-between mt-4">
-        <p className="text-lg">결제금액</p>
-        <p className="text-xl font-bold">{currency(price.final)} 원</p>
+        <p className="text-lg font-bold">총 결제금액</p>
+        <p className="text-xl font-bold text-red-500">{currency(price.final)} 원</p>
       </div>
     </div>
   );
